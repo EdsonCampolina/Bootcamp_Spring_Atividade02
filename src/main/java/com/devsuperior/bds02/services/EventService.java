7ea3c1321b2dba5e349008c610e66ef5bb3bf3b3
EdsonCampolina <ejcsengsoft@gmail.com>
2024-02-10 21:44:26 -0300

    Subindo tarefa

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,42 @@
+package com.devsuperior.bds02.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+
+	@Autowired
+	private CityService cityService;
+
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Optional<Event> obj = repository.findById(id);
+			Event entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+			entity.setName(dto.getName());
+			entity.setDate(dto.getDate());
+			entity.setUrl(dto.getUrl());
+			entity.setCity(cityService.findById(dto.getCityId()));
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+}
